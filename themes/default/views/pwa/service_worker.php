2e71a1e311b1b5e3adef1834a76c7d7dd64d29be
Oliver Servín <oliverds@users.noreply.github.com>
2021-12-13 11:53:31 -0600

    remove cached page handling when pwa enabled

--- themes/default/views/pwa/service_worker.php
+++ themes/default/views/pwa/service_worker.php
@@ -30,26 +30,7 @@ function onActivate(event) {
   );
 }
 
-// Borrowed from https://github.com/TalAter/UpUp
-function onFetch(event) {
-  event.respondWith(
-    // try to return untouched request from network first
-    fetch(event.request).catch(function() {
-      // if it fails, try to return request from the cache
-      return caches.match(event.request).then(function(response) {
-        if (response) {
-          return response;
-        }
-        // if not found in cache, return default offline content for navigate requests
-        if (event.request.mode === 'navigate' ||
-          (event.request.method === 'GET' && event.request.headers.get('accept').includes('text/html'))) {
-          //console.log('[Serviceworker]', "Fetching offline content", event);
-          return caches.match('/offline.html');
-        }
-      })
-    })
-  );
-}
+function onFetch(event) {}
 
 self.addEventListener('install', onInstall);
 self.addEventListener('activate', onActivate);
