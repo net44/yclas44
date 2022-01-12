5aaebe51470b261c58e20bc0a7ac364c1fd3b43a
Oliver Servín <oliverds@users.noreply.github.com>
2022-01-12 10:46:32 -0600

    4.4.0

--- oc/classes/core.php
+++ oc/classes/core.php
@@ -16,7 +16,7 @@ class Core {
      * OC version
      * @var string
      */
-    const VERSION = '4.3.0';
+    const VERSION = '4.4.0';
 
     /**
      * @var string used to populate data from valid domain
