abb0b2a9730aef87374419cba1bcd17456672e4f
Oliver Servín <oliverds@users.noreply.github.com>
2022-03-22 12:09:54 -0600

    Advertisements to Ads

--- themes/default/views/oc-panel/layouts/_nav.php
+++ themes/default/views/oc-panel/layouts/_nav.php
@@ -8,7 +8,7 @@
     <h3 class="px-2 text-xs leading-4 font-semibold text-blue-500 uppercase tracking-wider">
         <?= __('Manage') ?>
     </h3>
-    <?= Theme::admin_sidebar_link(__('Advertisements'), 'ad', 'index', '<svg fill="none" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" stroke="currentColor" viewBox="0 0 24 24"><path d="M8 7v8a2 2 0 002 2h6M8 7V5a2 2 0 012-2h4.586a1 1 0 01.707.293l4.414 4.414a1 1 0 01.293.707V15a2 2 0 01-2 2h-2M8 7H6a2 2 0 00-2 2v10a2 2 0 002 2h8a2 2 0 002-2v-2"></path></svg>') ?>
+    <?= Theme::admin_sidebar_link(__('Ads'), 'ad', 'index', '<svg fill="none" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" stroke="currentColor" viewBox="0 0 24 24"><path d="M8 7v8a2 2 0 002 2h6M8 7V5a2 2 0 012-2h4.586a1 1 0 01.707.293l4.414 4.414a1 1 0 01.293.707V15a2 2 0 01-2 2h-2M8 7H6a2 2 0 00-2 2v10a2 2 0 002 2h8a2 2 0 002-2v-2"></path></svg>') ?>
     <? if (in_array(core::config('general.moderation'), Model_Ad::$moderation_status)): ?>
         <?= Theme::admin_sidebar_link(__('Moderation'), 'ad', 'moderate', '<svg fill="none" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" viewBox="0 0 24 24" stroke="currentColor"><path d="M3 21v-4m0 0V5a2 2 0 012-2h6.5l1 1H21l-3 6 3 6h-8.5l-1-1H5a2 2 0 00-2 2zm9-13.5V9"></path></svg>') ?>
     <? endif ?>
